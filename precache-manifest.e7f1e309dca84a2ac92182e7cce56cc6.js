14d5cf1ded8f3410155592e8640900348fd60f9a
Manish <korangamanishsingh2002@gmail.com>
2023-01-23 22:49:59 +0530

    Updates

---
+++ precache-manifest.e7f1e309dca84a2ac92182e7cce56cc6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46aa2c3a5e61bbde89776d3dda0f7ee",
+    "url": "/NoteKeeper/index.html"
+  },
+  {
+    "revision": "2ec50e8d04ad6520f3e0",
+    "url": "/NoteKeeper/static/js/2.86d630e7.chunk.js"
+  },
+  {
+    "revision": "7c3ce223fd6dea193371",
+    "url": "/NoteKeeper/static/js/main.45426eb0.chunk.js"
+  },
+  {
+    "revision": "c918c98cf5c2342738da",
+    "url": "/NoteKeeper/static/js/runtime-main.1cfe757c.js"
+  }
+]);
